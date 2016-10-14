cb7894b4fe83f95520fc2e2aab4db4b850291437
Claassens <Tim.Claassens@ordina.nl>
2016-10-14 11:21:14 +0200

    altered db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var db = mysql.createPool({
-    host     : '127.0.0.1',
-    user     : 'cnc',
+    host     : 'db4free.net',
+    user     : 'cnc_2',
     password : '18943318f90e38630d9c8d81eb81dedfdb15ec67e6479e209fbf529e6fb40333',
-    database : 'code_and_comedy',
+    database : 'code_and_comedy_',
     connectionLimit: 10
 });
 
